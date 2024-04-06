ef1a4d92bbe8e384de6612778661474e5c41a2b9
samir-dadah <166208420+samir-dadah@users.noreply.github.com>
2024-04-06 05:34:04 -0300

    Create set_the_parameters.py

---
+++ set_the_parameters.py
@@ -0,0 +1,8 @@
+# Parameters used in the Dataset, on which YOLOv3 was pretrained
+anchors = [[116,90, 156,198, 373,326], [30,61, 62,45, 59,119], [10,13, 16,30, 33,23]]
+ 
+# define the expected input shape for the model
+WIDTH, HEIGHT = 416, 416
+ 
+# define the probability threshold for detected objects
+class_threshold = 0.3
